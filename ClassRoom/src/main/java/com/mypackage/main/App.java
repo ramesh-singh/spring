0f260616b7972a9f851e151fd3e960f09b7e94bb
br809e <br809e@att.com>
2018-05-15 16:55:59 -0500

    Added code to add classname in the getBean method.

--- ClassRoom/src/main/java/com/mypackage/main/App.java
+++ ClassRoom/src/main/java/com/mypackage/main/App.java
@@ -1,16 +1,23 @@
 package com.mypackage.main;
 
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 import com.mypackage.service.ClassRoomService;
 
 public class App{
     public static void main( String[] args ){
     	
-    	ApplicationContext context= new ClassPathXmlApplicationContext("classpath:spring/service.xml", "classpath:spring/model.xml");
-    	ClassRoomService classRoomService= (ClassRoomService) context.getBean("classRoomService");
+    	//ApplicationContext context= new ClassPathXmlApplicationContext("classpath:spring/service.xml", "classpath:spring/model.xml");
+    	//ClassRoomService classRoomService= context.getBean("classRoomService", ClassRoomService.class);
     	
+    	GenericApplicationContext context= new GenericApplicationContext();
+    	new XmlBeanDefinitionReader(context).loadBeanDefinitions("classpath:spring/service.xml", "classpath:spring/model.xml");
+    	context.refresh();
+    	
+    	ClassRoomService classRoomService= context.getBean("classRoomService", ClassRoomService.class);
     	classRoomService.teaching();
     }
     
